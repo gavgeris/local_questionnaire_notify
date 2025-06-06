4378deb3fd1d047df5e66c8492fa4cac580a33a4
george <gavgeris@gmail.com>
2025-06-06 12:51:06 +0300

    include user's sumbissions

--- lang/en/local_questionnaire_notify.php
+++ lang/en/local_questionnaire_notify.php
@@ -1,12 +1,19 @@
 <?php
+// File: lang/en/local_questionnaire_notify.php
 
 $string['pluginname'] = 'Questionnaire Submission Notifier';
 $string['emailsubject'] = 'Confirmation of your questionnaire submission in course: {$a}';
-$string['emailbody'] = 'Dear {$a->firstname},
+$string['emailbody'] = 'Dear {$a->firstname} {$a->lastname},
 
-Thank you for submitting the questionnaire for the course: "{$a->coursename}".
+Thank you for submitting the questionnaire "{$a->questionnaire_name}" for the course: "{$a->coursename}".
 
-We have received your application and will contact you shortly.
+SUBMISSION DETAILS:
+Submitted on: {$a->submission_date}
+
+YOUR RESPONSES:
+{$a->responses}
+
+We have received your submission and will contact you shortly.
 
 Best regards,
-Moodle Team';
+Course Administration Team';
